1ad71f2bd12b46d4d29f49f96c71c438967c522e
kxx935 <COco@12345>
2020-08-31 15:47:26 +0800

    fix bugs

--- functional_test.py
+++ functional_test.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
 import time
 import unittest
 
@@ -10,12 +11,8 @@ class NewVisitorTest(unittest.TestCase):
         self.url = 'http://127.0.0.1:8000'
         self.url_cv = 'http://127.0.0.1:8000/cv'
         self.url_cv_new = 'http://127.0.0.1:8000/cv/new_cv'
-        # create superuser
-        # self.user = User.objects.create_superuser(
-        #     username = 'test',
-        #     email = 'test@test.com',
-        #     password = 'test123'
-        # )
+
+
 
     def tearDown(self):
         self.browser.quit()
@@ -40,9 +37,62 @@ class NewVisitorTest(unittest.TestCase):
         time.sleep(1)
         self.assertTrue(self.browser.find_element_by_tag_name("input"))
 
+    def test_2_cv_page_as_superuser(self):
+        self.browser.get(self.url_cv_new)
 
+        #login
+        username = self.browser.find_element_by_name('username')
+        username.send_keys('test')
+        password = self.browser.find_element_by_name('password')
+        password.send_keys('ariescoco123')
+        password.send_keys(Keys.ENTER)
 
+        #add to info
+        textarea = self.browser.find_element_by_id('id_text')
+        textarea.send_keys('test info')
+        addinfo = self.browser.find_element_by_name('info')
+        addinfo.click()
+        # time.sleep(10)
 
+        #add to education
+        self.browser.get(self.url_cv_new)
+        textarea1 = self.browser.find_element_by_id('id_text')
+        textarea1.send_keys('test education')
+        addinfo = self.browser.find_element_by_name('education')
+        addinfo.click()
+
+        #add to work
+        self.browser.get(self.url_cv_new)
+        textarea = self.browser.find_element_by_id('id_text')
+        
+        textarea.send_keys('test work')
+        addinfo = self.browser.find_element_by_name('work')
+        addinfo.click()
+
+        #add to skills
+        self.browser.get(self.url_cv_new)
+        textarea = self.browser.find_element_by_id('id_text')
+        
+        textarea.send_keys('test skills')
+        addinfo = self.browser.find_element_by_name('skills')
+        addinfo.click()
+
+        #add to voluneering
+        self.browser.get(self.url_cv_new)
+        textarea = self.browser.find_element_by_id('id_text')
+        
+        textarea.send_keys('test voluneering')
+        addinfo = self.browser.find_element_by_name('voluneering')
+        addinfo.click()
+
+        time.sleep(10)
+
+        test_contexts = self.browser.find_elements_by_tag_name('p')
+        self.assertIn('test info',[test_context.get_attribute('textContent') for test_context in test_contexts])
+        self.assertIn('test education',[test_context.get_attribute('textContent') for test_context in test_contexts])
+        self.assertIn('test work',[test_context.get_attribute('textContent') for test_context in test_contexts])
+        self.assertIn('test skills',[test_context.get_attribute('textContent') for test_context in test_contexts])
+        self.assertIn('test voluneering',[test_context.get_attribute('textContent') for test_context in test_contexts])
 
 
 if __name__  == '__main__':
